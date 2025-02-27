a5964848a54ad9b2c3aceeddceb5e2a4a5bc8d1a
JACM-INF <jacminf@gmail.com>
2025-02-27 17:27:39 +0100

    Cambios en la rama secundaria

--- src/main/java/es/daw/backend/controller/ProductController.java
+++ src/main/java/es/daw/backend/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+//AÑADO COMENTARIOS EN LA RAMA SECUNDARIA
 @RestController
 @RequestMapping("/products")
 public class ProductController {
